cd7a270496dca3d36881fc7136ccbccd60f09076
Khairul Nizam <jongthechemist@hotmail.com>
2020-06-09 11:44:44 +0800

    Route: Add POST and PATCH /order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch } from '@nestjs/common';
 import { generateRandomOrder } from './order.mock';
 import { Order } from './order.model';
+import { generateId } from 'src/helpers/generate-id';
 
 @Controller('order')
 export class OrderController {
+
+  @Post()
+  async createOrder(@Body() order: Order): Promise<Order> {
+    order.id = generateId();
+    let createdOrder = await this.saveOrderToDB(order);
+    return createdOrder
+  }
 
   @Get(':id')
   async getOrder(@Param('id') orderId): Promise<Order> {
@@ -11,9 +19,29 @@ export class OrderController {
     return order
   }
 
+  @Patch(':id')
+  async updateOrder(@Param('id') orderId, @Body() newValues): Promise<Order> {
+    let order = await this.updateOrderInDB(orderId, newValues);
+    return order
+  }
+
+  async saveOrderToDB(order: Order): Promise<Order> {
+    return new Promise<Order>((resolve) => {
+      resolve(order);
+    })
+  }
+
   async getOrderFromDB(orderId: string): Promise<Order> {
     return new Promise<Order>((resolve) => {
       resolve(generateRandomOrder(orderId));
+    })
+  }
+  
+  async updateOrderInDB(orderId: string, newValues: Order): Promise<Order> {
+    return new Promise<Order>((resolve) => {
+      let order = generateRandomOrder(orderId);
+      Object.assign(order, newValues);
+      resolve(order);
     })
   }
 }
